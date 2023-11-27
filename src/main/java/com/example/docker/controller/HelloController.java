ce1443e60e8f405968f8d04bb27f3d439a03957a
Akmal <Akmaldavlatov85@mail.ru>
2023-11-27 11:34:17 +0500

    ddd3

---
+++ src/main/java/com/example/docker/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/salom")
+public class HelloController {
+
+    @GetMapping
+    public String salom(){
+        return "salom akmal akani apisi";
+    }
+}
